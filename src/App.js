ade6c259e546aac6ffecbf55669a639f48a8e5f8
niemand20 <zahid17290@gmail.com>
2020-08-20 19:17:32 +0100

    html inside js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var  person = {
+    name : "Mahfuz",
+    job : "Singer"
+  }
+  var person2 = {
+    name: "eva rahman",
+    job: "kokila"
+  }
+  var style = {
+    color:'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +22,10 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        <h1  className = "" style = {style}>my heading: {person.name+" "+person.job}</h1>
         
+  <h1 style = {{backgroundColor :'cyan', color:'gray'}}>my heading: {person2.name+" "+person2.job}</h1>
+        <p>My First React Paragraph</p>
       </header>
     </div>
   );
