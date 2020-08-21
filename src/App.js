b722e6027619f7f7f458211c4456b3b4b02f79e5
niemand20 <zahid17290@gmail.com>
2020-08-21 04:46:01 +0100

    pass dynamic object

--- src/App.js
+++ src/App.js
@@ -3,18 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var  person = {
-    name : "Mahfuz",
-    job : "Singer"
-  }
-  var person2 = {
-    name: "eva rahman",
-    job: "kokila"
-  }
-  var style = {
-    color:'red',
-    backgroundColor: 'yellow'
-  }
+  const nayoks  = ['manna','jafor','salman','razzak']
   return (
     <div className="App">
       <header className="App-header">
@@ -22,13 +11,26 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <h1  className = "" style = {style}>my heading: {person.name+" "+person.job}</h1>
+        <Person name ={nayoks[1]} naika = 'Shabnoor' food = 'pizza'></Person>
+        <Person name = 'Manna' naika = 'Purnima' food = 'burger'></Person>
         
-  <h1 style = {{backgroundColor :'cyan', color:'gray'}}>my heading: {person2.name+" "+person2.job}</h1>
-        <p>My First React Paragraph</p>
       </header>
     </div>
   );
 }
+function Person(props){
+  const personStyle = {
+    border: '5px solid red',
+    margin: '10px',
+    padding: '10px'
+  }
+  console.log(props);
+  return (
+   <div style = {personStyle}>
+    <h1>Name: {props.name}</h1>
+  <h3>naikaName:{props.naika}</h3>
+  </div>)
+}
+
 
 export default App;
